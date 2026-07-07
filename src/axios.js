5c9151868d7483e5972f3c74b91c067c04ecb2b4
Gaurav Hundl <gauravjeetsingh7@gmail.com>
2026-07-07 17:39:32 +0530

    Fix: Point Axios to live Firebase backend

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:5001/clone-dac75/us-central1/api' 
+  // Put YOUR actual live cloud function URL here:
+  baseURL: 'https://us-central1-clone-dac75.cloudfunctions.net/api' 
 });
 
 export default instance;
