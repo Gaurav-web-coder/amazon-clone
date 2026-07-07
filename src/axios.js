2e2d8f3f4bc343f56eeaaee34413a48dba55e3f3
Gaurav Hundl <gauravjeetsingh7@gmail.com>
2026-07-07 18:57:40 +0530

    Update Axios to point to Render backend

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // Put YOUR actual live cloud function URL here:
-  baseURL: 'https://us-central1-clone-dac75.cloudfunctions.net/api' 
+  baseURL: 'https://amazon-clone-094f.onrender.com' 
 });
 
 export default instance;
